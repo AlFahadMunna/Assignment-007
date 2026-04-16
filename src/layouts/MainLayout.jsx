85dd1d6841fb2b21f678d2e72ad43fdfe84dd7c0
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-04-16 23:28:18 +0600

    main layout added

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Header />
+      <main className="container py-12 px-3 flex flex-col gap-5 pt-32">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
+export default MainLayout;

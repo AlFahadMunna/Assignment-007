8c0e25487ede513707243baf384e3b4efbaa9a45
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-04-17 09:52:16 +0600

    project done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,58 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import Timeline from "./pages/Timeline";
+import Stats from "./pages/Stats";
+import NotFound from "./components/NotFound";
+import Friend from "./pages/Friend";
+import MainProvider from "./providers/MainProvider";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "timeline",
+        Component: Timeline,
+      },
+      {
+        path: "stats",
+        Component: Stats,
+      },
+      {
+        path: "friend/:id",
+        loader: async ({ params }) => {
+          return fetch("/friends.json")
+            .then((res) => res.json())
+            .then(
+              (data) =>
+                data?.find((item) => item?.id === parseInt(params?.id)) || {},
+            );
+        },
+        Component: Friend,
+      },
+      {
+        path: "*",
+        Component: NotFound,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <MainProvider>
+      <Toaster />
+      <RouterProvider router={router} />
+    </MainProvider>
   </StrictMode>,
-)
+);
